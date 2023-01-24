322c85e548d071b2da3762f3ee998fd7f9a55adb
Julian Quispe <julianmql@hotmail.com>
2023-01-24 10:39:41 -0500

    feat: lesson#14 completed

---
+++ lib/User/repository/firebase_auth_api.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthAPI{
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<User> signIn() async{
+    GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
+    GoogleSignInAuthentication gSA = await googleSignInAccount!.authentication;
+
+    UserCredential userCredential  = await _auth.signInWithCredential(
+      GoogleAuthProvider.credential(
+        idToken: gSA.idToken,
+        accessToken: gSA.accessToken,
+      )
+    );
+
+    return userCredential.user!;
+
+  }
+
+}
